021ff19be0ae46c4225cbcf93008f525c3e4a468
MendicottSPS <23912@stpauls.qld.edu.au>
2023-01-27 12:43:51 +1000

    .

--- Unit1_Create_With_Code/mypython01.py
+++ Unit1_Create_With_Code/mypython01.py
@@ -4,12 +4,12 @@ This is a multi
 line comment
 """
 # Input asking user to input something and saying to a variable
-name = input("What is your name?")
-age = input("What is your age?")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
 # Processing
 # Python can calculate numbers
-age = 2023 + age
+age = 20 + age
 
 # Output
-print("Hi, ", +name, "Your age in 20 years will be", age)
+print("Hi, ", name, "Your age in 20 years will be", age)
