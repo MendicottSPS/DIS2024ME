c01c257424c8ecd3d8a762c875955f65cfe05ca6
MendicottSPS <23912@stpauls.qld.edu.au>
2023-02-01 08:45:56 +1000

    .

--- Unit1_Create_With_Code/count_letters.py
+++ Unit1_Create_With_Code/count_letters.py
@@ -1,1 +1,14 @@
-//this is a counter
+# this is a counter
+
+print ("Welcome to my book title counter!")
+
+runagain = "y"
+while runagain == "y":
+    answer = input("Do you want me to count the characters in the title of the book? ").lower()
+    if answer == "yes":
+        book = input("What is the title of the book? ")
+        length = str(len(book))
+        print(f"There are {len(book)} characters in the book title {book} including the spaces.")
+    else:
+        runagain = "n"
+        print("Ok see you next time!")
