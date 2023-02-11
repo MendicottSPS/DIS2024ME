bc4563f016a017ab00157f43953586e7dad34dfe
MendicottSPS <23912@stpauls.qld.edu.au>
2023-02-11 17:27:36 +1000

    .

--- Unit1_Create_With_Code/Guessing_Game/guess07.py
+++ Unit1_Create_With_Code/Guessing_Game/guess07.py
@@ -1,24 +1,37 @@
 # one of the last attempts using a txt file to store information about game users
 
 import random
+# allows the import of a random number
 
+
+# This is where the game will write the player's score in a txt file to store until they play the game again
 def write_to_file(name, score):
     with open("scores.txt", "a") as file:
         file.write(f"{name},{score}\n")
 
+
+# This is used to check if the user that is playing has played before, and if they have, it will return their score
 def get_previous_score(name):
     with open("scores.txt", "r") as file:
-        for line in file:
-            line_name, score = line.strip().split(",")
-            if line_name = name:
-                return int(score)
-            return None
+        # This part was difficult, each time I played the game, it was not telling me my most recent score
+        # I found a solution that splits the line into two 'parts' based on the comma
+        # The split method meant that parts[0] would contain the user's name and parts[1] would contain their score
+        lines = file.readlines()
+        lines.reverse()
+        for line in lines:
+            parts = line.strip().split(",")
+            # Here if the part[0] which is the user's name was equal to the name that had just been input,
+            # The function would return the score which is stored in parts[1]
+            if parts[0] == name:
+                return int(parts[1])
+    return None
+
 
 def game_start():
-    number = random.randint(1,100)
+    number = random.randint(1, 100)
     guesses_left = 0
     while guesses_left not in [7, 5, 3]:
-        difficulty = input("Select a difficulty to play (easy=7, medium=5, hard=3): ")
+        difficulty = input("Select a number of guesses to determine your difficulty (easy=7, medium=5, hard=3): ")
         try:
             guesses_left = int(difficulty)
         except ValueError:
@@ -30,7 +43,7 @@ def game_start():
     previous_score = get_previous_score(name)
 
     if previous_score is not None:
-        print(f"Last time you played, you scored {previous_score}.")
+        print(f"Hi {name}, welcome back! Last time you played, you scored {previous_score}.")
 
     while guesses_left > 0:
         guess = int(input("Guess a number here: "))
@@ -40,12 +53,13 @@ def game_start():
             write_to_file(name, score)
             break
         elif guess < number:
-            print("The number is higher.")
+            print(f"The number is higher. You have {guesses_left} guesses remaining.")
         else:
-            print("The number is lower.")
+            print(f"The number is lower. You have {guesses_left} guesses remaining.")
         guesses_left -= 1
 
-    if guesses_left == 0
+    if guesses_left == 0:
         print(f"You have run out of guesses! The number was {number}.")
+
 
 game_start()
