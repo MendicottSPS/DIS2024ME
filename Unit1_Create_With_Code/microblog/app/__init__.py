1fc94bc47ae024a088555116e7a8b9097e41ee4b
MendicottSPS <23912@stpauls.qld.edu.au>
2023-03-09 11:45:19 +1000

    .

--- Unit1_Create_With_Code/microblog/app/__init__.py
+++ Unit1_Create_With_Code/microblog/app/__init__.py
@@ -1,10 +1,18 @@
-
 from flask import Flask
-from Unit1_Create_With_Code.microblog.config import Config
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'a very long string'
 
-from Unit1_Create_With_Code.microblog.app import routes
+login = LoginManager(app)
+login.login_view = 'login'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
 
